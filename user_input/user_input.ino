adc64d6109b364c3084ad97ca657b0e7739206c7
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2020-11-04 19:38:22 -0500

    Changed user input to rolling average

--- user_input/user_input.ino
+++ user_input/user_input.ino
@@ -1,5 +1,4 @@
-//Need to make these rolling averages
-
+#include "RunningAverage.h"
 
 // defines pins numbers, doesn't have to be 9 and 10
 //any d i/o works fine
@@ -11,99 +10,17 @@ const int trigPin2 = 8;
 const int echoPin2 = 7;
 
 // distance between sensor and object
-int left_distance;
-int right_distance;
+//distance[0] == left
+//distance[1] == right
+int distances[2];
 
-
-//setup to be run once
-void setup() {
-pinMode(trigPin1, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin1, INPUT); // Sets the echoPin as an Input
-pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin2, INPUT); // Sets the echoPin as an Input
-Serial.begin(9600); // Starts the serial communication
-}
-
-//take the average of a certain number of sensor samples
-int smooth_distance_left(int num_samples) {
-  //all values from left sensor
-  int left_value = 0;
-  
-  int current_left_distance = 0;
-  
-  for (int i = 0; i < num_samples; i++) {
-    current_left_distance = sample_distance_left();
-    if (current_left_distance > 0 && current_left_distance < 25) {
-      left_value = left_value + sample_distance_left();
-      //Serial.print("Good Reading.\n");
-    } else {
-      //Serial.print("Bad Reading.");
-      //take another sample of the data if there was a bad read
-      //probably going to need to remove this
-      //but it works well to test averages
-      i--;
-    }
-    
-  }
-  return (left_value/num_samples);
-}
-
-int smooth_distance_right(int num_samples) {
- //all values from right sensor
-  int right_value = 0;
-  
-  int current_right_distance = 0;
-  for (int i = 0; i < num_samples; i++) {
-    current_right_distance = sample_distance_right();
-    if (current_right_distance > 0 && current_right_distance < 25) {
-      right_value = right_value + sample_distance_right();
-      //Serial.print("Good Reading.\n");
-    } else {
-      //Serial.print("Bad Reading.");
-      //take another sample of the data if there was a bad read
-      //probably going to need to remove this
-      //but it works well to test averages
-      i--;
-    }
-    
-  }
-  return (right_value/num_samples);
-}
-
-//get a distance from the sensor
-int sample_distance_left() {
-  int current_duration;
-  int current_distance;
-  // Clears the trigPin
-  digitalWrite(trigPin1, LOW);
-  delayMicroseconds(2);
-  
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin1, LOW);
-  
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  //wait max 50,000 microseconds before reading
-  //may have to change this value
-
-  //we can also switch the pin given in these functions to switch which side 
-  //of the bar each sensor corresponds to... >:)
-  current_duration = pulseIn(echoPin1, HIGH, 50000);
-  
-  // Calculating the distance
-  current_distance = current_duration*0.034/2;
-  
-  // Prints the distance on the Serial Monitor
-  //Serial.print("Distance: ");
-  //Serial.println(current_distance);
-
-  return current_distance;
-}
+RunningAverage left_sensor(20);
+RunningAverage right_sensor(20);
 
 int sample_distance_right() {
   int current_duration;
   int current_distance;
+  
   // Clears the trigPin
   digitalWrite(trigPin2, LOW);
   delayMicroseconds(2);
@@ -131,14 +48,48 @@ int sample_distance_right() {
   return current_distance;
 }
 
+//get a distance from the sensor
+int sample_distance_left() {
+  int current_duration;
+  int current_distance;
+  
+  digitalWrite(trigPin1, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW);
+ 
+  current_duration = pulseIn(echoPin1, HIGH, 50000);
+  current_distance = current_duration*0.034/2;
+
+  return current_distance;
+}
+
+void get_user_input(){
+  left_sensor.addValue(sample_distance_left);
+  right_sensor.addValue(sample_distance_right);
+  Serial.print("Left Distance: ");
+  Serial.println(left_sensor.getAverage());
+  Serial.print("\nRight Distance: ");
+  Serial.println(right_sensor.getAverage());
+}
+
+//setup to be run once
+void setup() {
+  //set up pins
+  pinMode(trigPin1, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin1, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin2, INPUT); // Sets the echoPin as an Input
+  //clear running averages
+  left_sensor.clear();
+  right_sensor.clear();
+  Serial.begin(9600); // Starts the serial communication
+}
 
 //to be repeated
 void loop() {
   //just for testing purposes
-  left_distance = smooth_distance_left(20);
-  right_distance = smooth_distance_right(20);
-  Serial.print("Left Distance: ");
-  Serial.println(left_distance);
-  Serial.print("\nRight Distance: ");
-  Serial.println(right_distance);
+  get_user_input();
 }
