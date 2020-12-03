d343293c5d464164e1504b5ff06a3123db82590a
Jan729 <46268176+Jan729@users.noreply.github.com>
2020-12-02 20:09:45 -0500

    Add code for 1 motor. 3 speeds for now

--- main/stepper_driver_draft/stepper_driver_draft.ino
+++ main/stepper_driver_draft/stepper_driver_draft.ino
@@ -1,32 +1,49 @@
 /***************************
-Code was modified from:
+  Code was modified from:
 
-SparkFun Easy Driver Basic Demo
-Toni Klopfenstein @ SparkFun Electronics
-March 2015
-Arduino 1.6.0
+  SparkFun Easy Driver Basic Demo
+  Toni Klopfenstein @ SparkFun Electronics
+  March 2015
+  Arduino 1.6.0
 
-https://github.com/sparkfun/Easy_Driver
-Simple demo sketch to demonstrate how 5 digital pins can drive a bipolar stepper motor,
-using the Easy Driver (https://www.sparkfun.com/products/12779). Also shows the ability to change
-microstep size, and direction of motor movement.
+  https://github.com/sparkfun/Easy_Driver
+  Simple demo sketch to demonstrate how 5 digital pins can drive a bipolar stepper motor,
+  using the Easy Driver (https://www.sparkfun.com/products/12779). Also shows the ability to change
+  microstep size, and direction of motor movement.
 
-Example based off of demos by Brian Schmalz (designer of the Easy Driver).
-http://www.schmalzhaus.com/EasyDriver/Examples/EasyDriverExamples.html
+  Example based off of demos by Brian Schmalz (designer of the Easy Driver).
+  http://www.schmalzhaus.com/EasyDriver/Examples/EasyDriverExamples.html
 
-The following code is for 1 stepper driver
-1 stepper driver can drive 1 motor and needs 5 arduino pins.
-We'll need to duplicate this code, and use 10 pins in total to drive 2 motors.
+  The following code is for 1 stepper driver
+  1 stepper driver can drive 1 motor and needs 5 arduino pins.
+  We'll need to duplicate this code, and use 10 pins in total to drive 2 motors.
  ***************************/
 
 //TODO: change pins to the actual pins we're using
 
+//EXPERIMENTAL VALUES - will need to adjust as we prototype
 #define CEILING 200 //max value of bar postion
+#define FLOOR 3 //min value of bar position. include a bit of space so it won't hit the actual floor
 #define STEP_INCR 1000 //number of steps to move at each iteration
-//at each iteration, either bar_pos++ or bar_pos--
 
-char user_input;
+//distance sensors will give the motors a number within the range [-3, 3]
+//-ve speed means "down", +ve speed means "up"
+#define STOP 0
+#define SLOW 1
+#define MED 2
+#define FAST 3
+
+//used to control delay. (MAXDELAY - FAST) gives a delay of 1
+//which is faster then (MAXDELAY - SLOW), which gives a delay of 3
+#define MAXDELAY 4
+
+int userInputLeft; //from distance sensor functions
+int userInputRight;
+
 int x; //looping variable. reused multiple times
+
+#define MAX_BAR_TILT 10 //maximum vertical slope of bar, aka barPosRight - barPosLeft
+int barTilt = 0;
 
 /******** variables for motor 1 *******/
 #define stp_1 2
@@ -34,70 +51,78 @@ int x; //looping variable. reused multiple times
 #define MS1_1 4
 #define MS2_1 5
 #define EN_1  6
-
-
-int bar_pos_left = 0; //position of the left side of the bar in steps
-//bar_pos_left = 0 means left side of bar is on floor
+int barPosLeft = 0; //position of the left side of the bar in steps
 
 /***************************************/
 
 /******** variables for motor 2 *******/
 
 //once 1 motor is working, duplicate code for motor 2
-
+int barPosRight = 0;
 
 /***************************************/
 
 void setup() {
   // put your setup code here, to run once:
+
+  //left motor setup
   pinMode(stp_1, OUTPUT);
   pinMode(dir_1, OUTPUT);
   pinMode(MS1_1, OUTPUT);
   pinMode(MS2_1, OUTPUT);
   pinMode(EN_1, OUTPUT);
 
+  //TODO: right motor setup
+
   resetEDPins(); //Reset all 10 Easy Driver pins to default states
 
   Serial.begin(9600); //Open Serial connection for debugging. remove when merging.
-  Serial.println("Enter number for control option:");
-  Serial.println("1. Step forward 1 step.");
-  Serial.println("2. Step backward 1 step.");
+  Serial.println("Enter number for control option:"); //LEFT MOTOR ONLY
+  //assume user enters 1 for normal, 2 for fast, 3 for fastest speed upwards
+  //-1, -2, and -3 move the bar downwards
 }
-
-
-//the sparkfun example has 4 demo functions,
-//but we only need "forward" and "backward"
-//Assume "step forward" translates to "move the bar up a little bit"
 
 //TODO: instead of using the serial monitor to control the motors, we'll need
 //to check the values from our distance sensors to decide which motor and which direction to move
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  while(Serial.available()){
-      user_input = Serial.read(); //Read user input and trigger appropriate function
-      digitalWrite(EN_1, LOW); //Pull enable pin low to allow motor control
-      if (user_input =='1')
-      {
-        if (bar_pos_left < CEILING) {
-         StepForwardDefault();
-         bar_pos_left++;
-        }
-        //else do nothing
-      }
-      else if(user_input =='2')
-      {
-        if(bar_pos_left > 0) {
-          ReverseStepDefault();
-          bar_pos_left--;
-        }
-      } else {
-        Serial.println("Invalid option entered.");
-      }
-      resetEDPins();
+  // FOR LEFT MOTOR ONLY
+  while (Serial.available()) {
+
+    userInputLeft = Serial.parseInt(); //Read user input and trigger appropriate function
+    Serial.print("Received: ");
+    Serial.print(userInputLeft);
+    Serial.println();
+    
+    digitalWrite(EN_1, LOW); //Pull enable pin low to allow motor control
+
+    if (userInputLeft > 0
+        && barPosLeft < CEILING
+        && barTilt > -MAX_BAR_TILT) {       //move left side of bar UP
+      moveStepper(true, dir_1, stp_1, MAXDELAY - userInputLeft);
+      barPosLeft += userInputLeft;
+    } else if (userInputLeft < 0
+               && barPosLeft > FLOOR
+               && barTilt < MAX_BAR_TILT) {     //move left side of bar DOWN
+      moveStepper(false, dir_1, stp_1, MAXDELAY - userInputLeft);
+      barPosLeft -= userInputLeft;
+    }
+
+    barTilt = barPosRight - barPosLeft;
+    
+    Serial.print("left bar pos = ");
+    Serial.print(barPosLeft);
+    Serial.println();
+    Serial.print("bar tilt = ");
+    Serial.print(barTilt);
+    Serial.println();
+    
+    resetEDPins();
   }
 }
 
 //Reset Easy Driver pins to default states
+//TODO: reset right motor pins
 void resetEDPins()
 {
   digitalWrite(stp_1, LOW);
@@ -107,33 +132,27 @@ void resetEDPins()
   digitalWrite(EN_1, HIGH);
 }
 
-//Default microstep mode function
-void StepForwardDefault()
+void moveStepper(bool goingUp, int dirPin, int stpPin, int stepperDelay)
 {
-  Serial.println("Moving forward at default step mode.");
-  digitalWrite(dir_1, LOW); //Pull direction pin low to move "forward"
-  for(x= 0; x<STEP_INCR; x++)  //Loop the forward stepping enough times for motion to be visible
-  {
-    digitalWrite(stp_1,HIGH); //Trigger one step forward
-    delay(1);
-    digitalWrite(stp_1,LOW); //Pull step pin low so it can be triggered again
-    delay(1);
-  }
-  Serial.println("Enter new option");
-  Serial.println();
-}
+  Serial.print("Moving stepper ");
 
-//Reverse default microstep mode function
-void ReverseStepDefault()
-{
-  Serial.println("Moving in reverse at default step mode.");
-  digitalWrite(dir_1, HIGH); //Pull direction pin high to move in "reverse"
-  for(x= 0; x<STEP_INCR; x++)  //Loop the stepping enough times for motion to be visible
+  if (goingUp) {
+    Serial.print("up with delay ");
+    digitalWrite(dirPin, LOW); //Pull direction pin low to move "forward"
+  } else {
+    Serial.print("down with delay ");
+    digitalWrite(dirPin, HIGH); //Pull direction pin high to move "backward"
+  }
+
+  Serial.print(stepperDelay, DEC);
+  Serial.println();
+  
+  for (x = 0; x < STEP_INCR; x++) //Loop the forward stepping enough times for motion to be visible
   {
-    digitalWrite(stp_1,HIGH); //Trigger one step
-    delay(1);
-    digitalWrite(stp_1,LOW); //Pull step pin low so it can be triggered again
-    delay(1);
+    digitalWrite(stpPin, HIGH); //Trigger one step forward
+    delay(stepperDelay);
+    digitalWrite(stpPin, LOW); //Pull step pin low so it can be triggered again
+    delay(stepperDelay);
   }
   Serial.println("Enter new option");
   Serial.println();
